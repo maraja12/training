419c9de37a1042e152cfc023436a1ba56509610a
Marija Rajic <marijarajic64@gmail.com>
2023-01-22 14:00:57 +0100

    Napravljen DatabaseSeeder, ubaceni factoryu bazu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Training;
+use App\Models\Equipment;
+use App\Models\Coach;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +24,56 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::truncate();
+        Coach::truncate();
+        Equipment::truncate();
+        Training::truncate();
+
+        User::factory(3)->create();
+
+        $coach1 = Coach::factory()->create();
+        $coach2 = Coach::factory()->create();
+        $coach3 = Coach::factory()->create();
+
+        $equipment1 = Equipment::factory()->create();
+        $equipment2 = Equipment::factory()->create();
+        $equipment3 = Equipment::factory()->create();
+
+        Training::factory()->create([
+            'coach_id' => $coach1->id,
+            'equipment_id' => $equipment1->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach1->id,
+            'equipment_id' => $equipment2->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach1->id,
+            'equipment_id' => $equipment3->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach2->id,
+            'equipment_id' => $equipment1->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach2->id,
+            'equipment_id' => $equipment2->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach2->id,
+            'equipment_id' => $equipment3->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach3->id,
+            'equipment_id' => $equipment1->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach3->id,
+            'equipment_id' => $equipment2->id
+        ]);
+        Training::factory()->create([
+            'coach_id' => $coach3->id,
+            'equipment_id' => $equipment3->id
+        ]);
     }
 }
