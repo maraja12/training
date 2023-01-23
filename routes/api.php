39d9d2ce5e6a7ab15d5ee0ba64fe7c3c199f058b
Marija Rajic <marijarajic64@gmail.com>
2023-01-23 15:00:01 +0100

    Napravljene api rute

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthorisationController;
+use App\Http\Controllers\TrainingController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\EquipmentTrainingController;
+use App\Http\Controllers\CoachController;
+use App\Http\Controllers\CoachTrainingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +22,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/coaches', [CoachController::class, 'index']);
+Route::get('/coaches/{id}', [CoachController::class, 'show']);
+
+Route::get('/equipments', [EquipmentController::class, 'index']);
+Route::get('/equipments/{id}', [EquipmentController::class, 'show']);
+
+Route::get('/trainings', [TrainingController::class, 'index']);
+Route::get('/trainings/{id}', [TrainingController::class, 'show']);
+
+Route::resource('coaches.trainings', CoachTrainingController::class)
+    ->only(['index']);
+
+Route::resource('equipments.trainings', EquipmentTrainingController::class)
+    ->only(['index']);
+
+Route::post('/register', [AuthorisationController::class, 'register']);
+Route::post('/login', [AuthorisationController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+Route::resource('/coaches', CoachController::class)
+    ->only(['store', 'update', 'destroy']);
+
+Route::resource('/equipments', EquipmentController::class)
+    ->only(['store', 'update', 'destroy']);
+
+Route::resource('/trainings', TrainingController::class)
+    ->only(['store', 'update', 'destroy']);
+
+Route::post('/logout', [AuthorisationController::class, 'logout']);
+
 });
