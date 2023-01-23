fc5d3c36e76bf906317782cf3e5773878e5fc8be
Marija Rajic <marijarajic64@gmail.com>
2023-01-23 15:22:13 +0100

    Ispravljena greska kod namespace-a

--- app/Http/Resources/EquipmentResource.php
+++ app/Http/Resources/EquipmentResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Resources\Equipment;
+namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
--- app/Http/Resources/TrainingResource.php
+++ app/Http/Resources/TrainingResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Resources\Training;
+namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
